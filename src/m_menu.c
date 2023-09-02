a6bad2ea159d61271f0e2eefbcddebf96947fcb8
P-AS <43502756+PandaSRC@users.noreply.github.com>
2023-09-02 03:57:51 -0400

    Show FPS Cap option in Video Options even with out HW renderer

--- src/m_menu.c
+++ src/m_menu.c
@@ -1386,10 +1386,12 @@ static menuitem_t OP_VideoOptionsMenu[] =
 	{IT_STRING | IT_CVAR, NULL, "Clear Before Redraw",       &cv_homremoval,      195},
 	{IT_STRING | IT_CVAR, NULL, "Show \"FOCUS LOST\"",       &cv_showfocuslost,   200},
 
-#ifdef HWRENDER
 	{IT_HEADER, NULL, "Renderer", NULL, 208},
+#ifdef HWRENDER
 	{IT_CALL | IT_STRING, NULL, "OpenGL Options...",         M_OpenGLOptionsMenu, 214},
 	{IT_STRING | IT_CVAR, NULL, "FPS Cap",                   &cv_fpscap,          219},
+#else
+	{IT_STRING | IT_CVAR, NULL, "FPS Cap",                   &cv_fpscap,          214},
 #endif
 };
 
